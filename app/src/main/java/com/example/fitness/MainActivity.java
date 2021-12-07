309a68fe9e476d2c7efb98436089c815781be9f5
Erick Weigner Freitas Cunha <erickweiner@gmail.com>
2021-12-06 21:06:00 -0300

    Adicionando botao imc com icone e criando imcActivity

--- app/src/main/java/com/example/fitness/MainActivity.java
+++ app/src/main/java/com/example/fitness/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.fitness;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View btnImc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnImc = findViewById(R.id.btn_imc);
+
+        btnImc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
