68a11bccd0b975c2f4e1ca90c241dbcbbf5fd712
Erick Weigner Freitas Cunha <erickweiner@gmail.com>
2021-12-14 21:06:41 -0300

    Working RecyclerView

--- app/src/main/java/com/example/fitness/MainActivity.java
+++ app/src/main/java/com/example/fitness/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.fitness;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -18,7 +19,6 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    // private View btnImc;
     private RecyclerView recyclerMain;
 
     @Override
@@ -26,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        btnImc = findViewById(R.id.btn_imc);
         recyclerMain = findViewById(R.id.recycler_main);
 
 
@@ -35,24 +34,34 @@ public class MainActivity extends AppCompatActivity {
         mainItems.add(new MainItem(2, R.drawable.ic_baseline_visibility_24, R.string.label_tmb, R.color.anonymuse));
 
         // mosaic, grid ou linear(horizontal | vertical)
-        recyclerMain.setLayoutManager(new LinearLayoutManager(this));
+        recyclerMain.setLayoutManager(new GridLayoutManager(this, 2));
 
         MainAdapter adapter = new MainAdapter(mainItems);
+        adapter.setListener( id -> {
+            switch (id) {
+                case 1:
+                    startActivity(new Intent(getBaseContext(), ImcActivity.class));
+                    break;
+                case 2:
+                    startActivity(new Intent(getBaseContext(), TmbActvity.class));
+                    break;
+            }
+        });
+
         recyclerMain.setAdapter(adapter);
-
-
-//        btnImc.setOnClickListener(view -> {
-//            Intent intent = new Intent(getBaseContext(), ImcActivity.class);
-//            startActivity(intent);
-//        });
     }
 
-    private class MainAdapter extends RecyclerView.Adapter<MainViewHolder> {
+    private class MainAdapter extends RecyclerView.Adapter<MainAdapter.MainViewHolder> {
 
         private List<MainItem> mainItems;
+        private OnItemClickListener listener;
 
         public MainAdapter(List<MainItem> mainItems) {
             this.mainItems = mainItems;
+        }
+
+        public void setListener(OnItemClickListener listener) {
+            this.listener = listener;
         }
 
         @NonNull
@@ -71,22 +80,26 @@ public class MainActivity extends AppCompatActivity {
         public int getItemCount() {
             return mainItems.size();
         }
-    }
 
-    private class MainViewHolder extends RecyclerView.ViewHolder {
+        private class MainViewHolder extends RecyclerView.ViewHolder {
 
-        public MainViewHolder(@NonNull View itemView) {
-            super(itemView);
-        }
+            public MainViewHolder(@NonNull View itemView) {
+                super(itemView);
+            }
 
-        public void bind(MainItem item) {
-            TextView txtName = itemView.findViewById(R.id.item_txt_name);
-            ImageView imgIcon = itemView.findViewById(R.id.item_img_icon);
-            LinearLayout container = (LinearLayout) itemView;
+            public void bind(MainItem item) {
+                TextView txtName = itemView.findViewById(R.id.item_txt_name);
+                ImageView imgIcon = itemView.findViewById(R.id.item_img_icon);
+                LinearLayout btnImc = (LinearLayout) itemView.findViewById(R.id.btn_imc);
 
-            txtName.setText(item.getTextStringId());
-            imgIcon.setImageResource(item.getDrawableId());
-            container.setBackgroundColor(item.getColor());
+                btnImc.setOnClickListener(view -> {
+                    listener.onClick(item.getId());
+                });
+
+                txtName.setText(item.getTextStringId());
+                imgIcon.setImageResource(item.getDrawableId());
+                btnImc.setBackgroundColor(item.getColor());
+            }
         }
     }
 }
